aa0920b1d54a266f6c0195ec419809841f7e1c5c
Mamessile Kiman <sile.kiman@yahoo.com>
2019-10-30 22:29:05 -0400

    uploading the script to generate the random password file

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 //declare variables
 
+var pwdLength = prompt("Please enter the lenght of your password, between 8 & 128 characters");
+var specialChar = "~!@#$%^&*()__-+=?{}[]||\/";
+var pwdNumber = "0123456789";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowcaseChar = "abcdefghijklmnopqrstuvwxyz";
+var Char = "";
 
-var userInput = {
-    pwdLenght: prompt("Please enter the lenght of your password, between 8 & 128 characters"),
+// Function to validate user's input
+function validUerinput() {
+    while (pwdLength <8 || pwdLength> 128 || pwdLength === " ") {
 
-    specialChar: confirm("Do you want it to contain special characters?"),
-
-    pwdNumber: confirm("Do you want it to contain  numbers?"),
-
-    lowcaseChar: confirm("Do you want it to contain lowercase characters?"),
-
-    uppercaseChar: confirm("Do you want it to contain uppercase characters?")
-
-
-}
-
-
-function validateInput() {
-
-    while (userInput !== "") {
-
-        value = userInput.pwdLenght;
-        console.log(value)
-
-        if (value > 0) {
-            value1 = userInput.specialChar;
-            return value1;
-            console.log(value1)
-        }
-        else if (value1 === true) {
-            value2 = userInput.lowcaseChar;
-            return value2;
-            console.log(value2)
-
-        }
-
-
+            pwdLength = prompt("Please enter a valid password, between 8 & 128 characters");
+        
     }
+    var special = confirm("Do you want your password to contain special characters?")
+    if (special === true) {
+        Char += specialChar;
+    }
+    // console.log(Char)
+
+    var Num = confirm("Do you want your password to contain  numbers?")
+    if (Num === true) {
+        Char += pwdNumber;
+    }
+    // console.log(Char)
+
+
+    var low = confirm("Do you want your password to contain lowercase characters?")
+    if (low === true) {
+        Char += lowcaseChar;
+    }
+    // console.log(Char)
+
+    var upper = confirm("Do you want your password to contain uppercase characters?")
+    if (upper === true) {
+        Char += uppercaseChar;
+    }
+    // console.log(Char)
 
 }
-console.log(validateInput())
+validUerinput();
+
+    // Funtion to generate the random password
+    function generatePwd(Char, pwdLength) {
+        var password = "";
+        for (i = 0; i <= pwdLength; i++) {
+            password += Char.charAt(Math.floor(Math.random() * Char.length));
+        }
+        console.log(password);
+        return password;
+    }
+generatePwd(Char, pwdLength);
